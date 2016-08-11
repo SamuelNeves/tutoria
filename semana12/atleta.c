5d550732e758241a897dc672ff093036403f1745
SamuelNeves <samuel.neves@live.com>
2016-08-11 20:28:56 -0300

    up

--- semana12/atleta.c
+++ semana12/atleta.c
@@ -1,39 +1,50 @@
 #include <stdio.h>
 
 
-typedef struct atleta{
-  char nome[50];
-  char esporte[50];
-  int idade;
-  float altura;
+typedef struct atleta {
+        char nome[50];
+        char esporte[50];
+        int idade;
+        float altura;
 } Atleta;
 
 int main(){
-  Atleta vet[5];
-  int i;
-  //Preenchendo structs
-  for(i=0;i<5;i++){
-    printf("Insira o nome do atleta %d\n",i+1 );
-    scanf("%s",&vet[i].nome );
-    printf("Insira o esporte \n" );
-    scanf("%s",&vet[i].esporte );
-    printf("Insira a idade: \n" );
-    scanf("%d",&vet[i].idade );
-    printf("Insira a altura\n" );
-    scanf("%f",&vet[i].altura );
-  }
+        Atleta vet[5];
+        int i;
+        //assumo que o atleta mais alto e o atleta mais velho é o da posição zero
+        int maisAlto=0,maisVelho=0;
+        //Preenchendo structs
+        for(i=0; i<5; i++) {
+                printf("Insira o nome do atleta %d\n",i+1 );
+                scanf("%s",&vet[i].nome );
+                printf("Insira o esporte \n" );
+                scanf("%s",&vet[i].esporte );
+                printf("Insira a idade: \n" );
+                scanf("%d",&vet[i].idade );
+                printf("Insira a altura\n" );
+                scanf("%f",&vet[i].altura );
+        }
 
-  //imprimindo structs
+        //imprimindo structs
+        for(i=1; i<5; i++) {
+                if(vet[i].idade>vet[maisVelho].idade) {
+                        maisVelho=i;
+                }
+                if(vet[i].altura>vet[maisAlto].altura) {
+                        maisAlto=i;
+                }
+        }
+        printf("Atleta mais velho\n" );
+        printf("Nome: %s\n",vet[maisVelho].nome );
+        printf("Esporte: %s\n",vet[maisVelho].esporte );
+        printf("Idade: %d \n",vet[maisVelho].idade );
+        printf("Altura: %f\n",vet[maisVelho].altura );
 
-  for(i=0;i<5;i++){
-    printf("Aluno %d \n",i+1 );
-    printf("Nome: %s\n",vet[i].nome );
-    printf("Esporte: %s\n",vet[i].esporte );
-    printf("Idade: %d \n",vet[i].idade );
-    printf("Altura: %f\n",vet[i].altura );
-  }
+        printf("Atleta mais alto\n" );
+        printf("Nome: %s\n",vet[maisAlto].nome );
+        printf("Esporte: %s\n",vet[maisAlto].esporte );
+        printf("Idade: %d \n",vet[maisAlto].idade );
+        printf("Altura: %f\n",vet[maisAlto].altura );
 
-
-
-  return 0;
+        return 0;
 }
